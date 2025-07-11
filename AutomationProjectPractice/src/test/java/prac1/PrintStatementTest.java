0b9ac16930d7d447fc7e6be39a9c7cf02fa994e5
mathmy12 <49333844+mathmy12@users.noreply.github.com>
2025-07-11 16:20:32 +0530

    Using maven command

--- AutomationProjectPractice/src/test/java/prac1/PrintStatementTest.java
+++ AutomationProjectPractice/src/test/java/prac1/PrintStatementTest.java
@@ -6,6 +6,16 @@ public class PrintStatementTest {
    @Test
    public void runThroughJenkinsTest() {
 	   System.out.println("we are executing by using jenkins");
+	   String URL = System.getProperty("url");
+	   String BROWSER = System.getProperty("browser");
+	   String USERNAME = System.getProperty("username");
+	   String PASSWARD = System.getProperty("passward");
+	   
+	   System.out.println(" url is: "+URL);
+	   System.out.println(" browser is: "+BROWSER);
+	   System.out.println(" Username is: "+USERNAME);
+	   System.out.println(" Passward is: "+ PASSWARD);
+	   
    }
    
    @Test
